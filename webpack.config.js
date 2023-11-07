28bd0753473f3a4925608014cc8f80a1fa992ef4
Frit027 <Frit_027@mail.ru>
2023-11-07 22:25:09 +0300

    created webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'build.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    target: ['web', 'es5'],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-react',
+                                ['@babel/preset-env', {
+                                    targets: {
+                                        ie: '10',
+                                        edge: '17',
+                                        firefox: '60',
+                                        chrome: '60',
+                                        safari: '12',
+                                    },
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                }],
+                            ],
+                        },
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            compilerOptions: {
+                                noEmit: false,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx'],
+    },
+};
