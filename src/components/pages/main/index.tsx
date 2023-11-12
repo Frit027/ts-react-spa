52b31158027a547c61821b56a900a2558f442cf3
Frit027 <Frit_027@mail.ru>
2023-11-12 21:15:07 +0300

    moved file

---
+++ src/components/pages/main/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { texts } from './constants';
+
+/**
+ * Главная страница сайта
+ * @constructor
+ */
+const Main = () => {
+    return (
+        <h1>{texts.text}</h1>
+    );
+};
+
+export default Main;
