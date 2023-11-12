1a74154231ce488ecadad4400e0aadae18d02cc3
Frit027 <Frit_027@mail.ru>
2023-11-13 00:44:23 +0300

    added JSDoc and get-route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,23 @@
 import { createServer, Response } from 'miragejs';
 import { v4 as uuidv4 } from 'uuid';
-import { db } from './db';
+import { users, data } from './db';
 
 /**
- * Fake-сервер
+ * Fake-сервер, предоставляющий API для GET- и POST-запросов
  */
 export const makeServer = () => createServer({
     routes() {
+        this.get('/api/data', () => ({ data }));
+
         this.post('/api/login', (schema, request) => {
             const { login, password } = JSON.parse(request.requestBody);
-            const user = db.find((u) => u.login === login);
+            const user = users.find((u) => u.login === login);
 
             if (user && user.password === password) {
                 return { token: uuidv4() };
             }
 
-            return new Response(401, {}, 'Ошибка авторизации');
+            return new Response(401, {}, 'Неверный логин или пароль.');
         });
     },
 });
