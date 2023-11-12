3cca18065d013e974c2e8cc65be6b152d120829a
Frit027 <Frit_027@mail.ru>
2023-11-12 21:14:48 +0300

    added compare functions

---
+++ src/components/pages/data-browsing/utils.ts
@@ -0,0 +1,33 @@
+import { TChildren } from './interfaces';
+
+/**
+ * Сравнение двух дочерних элементов родителя по их имени
+ * @param a - Первый ребёнок
+ * @param b - Второй ребёнок
+ * @returns {number} Результат сравнения двух строк
+ */
+export const compareAsc = (a: TChildren, b: TChildren): number => {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+    return 0;
+};
+
+/**
+ * Сравнение двух дочерних элементов родителя по их имени
+ * @param a - Первый ребёнок
+ * @param b - Второй ребёнок
+ * @returns {number} Результат сравнения двух строк
+ */
+export const compareDesc = (a: TChildren, b: TChildren): number => {
+    if (a.name < b.name) {
+        return 1;
+    }
+    if (a.name > b.name) {
+        return -1;
+    }
+    return 0;
+};
