a826355e91b1c6a83de5b4a0cfbe697d7ea62077
Frit027 <Frit_027@mail.ru>
2023-11-12 19:30:36 +0300

    added comments

--- src/components/providers/auth-provider/interfaces.ts
+++ src/components/providers/auth-provider/interfaces.ts
@@ -1,16 +1,22 @@
 import React from 'react';
 
 export type TAuthProviderProps = {
+    // передаваемый компонент в провайдер
     children: React.ReactNode,
 };
 
 export type TData = {
+    // токен пользователя
     token: string,
 };
 
 export type TAuthContext = {
+    // сохранения данных пользователя
     logIn: (data: TData) => void,
+
+    // удаление данных пользователя
     logOut: () => void,
+
+    // проверка на авторизацию
     isLoggedIn: () => boolean,
-    getToken: () => string,
 };
