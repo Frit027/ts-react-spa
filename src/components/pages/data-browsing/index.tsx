9c2f5686c05b6fc862f686807949d01ee6e8db8c
Frit027 <Frit_027@mail.ru>
2023-11-13 01:52:39 +0300

    added JSDoc, moved method

--- src/components/pages/data-browsing/index.tsx
+++ src/components/pages/data-browsing/index.tsx
@@ -1,28 +1,58 @@
-import React, {
-    useContext, useEffect, useState, ChangeEvent,
-} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../../providers/auth-provider';
-import { TTree } from '../../../server/interfaces';
-import { TChildren, TParent, TOrder } from './interfaces';
-import { compareAsc, compareDesc } from './utils';
+import { TTree, TParent } from './interfaces';
 import Parents from '../../elements/parents';
 import Children from '../../elements/children';
 
+/**
+ * Приватная страница сайта для просмотра информации с сервера
+ * @constructor
+ */
 const DataBrowsing = () => {
     const [tree, setTree] = useState<TTree[]>([]);
     const [parents, setParents] = useState<TParent[]>([]);
-    const [children, setChildren] = useState<TChildren[]>([]);
-    const [order, setOrder] = useState<TOrder>('asc');
-
+    const [parentKey, setParentKey] = useState<string>('');
     const { isLoggedIn } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const findNodeParents = () => {
+    /**
+     * Получаем данные по API через GET-запрос
+     */
+    const fetchData = async (): Promise<void> => {
+        try {
+            const { data } = await axios.get('/api/data');
+            setTree(data.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    /**
+     * При открытии страницы перенаправляем на страницу входа, если пользователь не авторизован
+     * Иначе загружаем данные с сервера
+     */
+    useEffect(() => {
+        if (!isLoggedIn()) {
+            navigate('/login');
+        }
+        fetchData();
+    }, []);
+
+    /**
+     * Ищем все родительские элементы
+     * @returns {TParent[]} Массив найденных родительских элементов
+     */
+    const findNodeParents = (): TParent[] => {
         const nodeParents: TParent[] = [];
 
-        const findParent = (nodes: TTree[], level = 1) => {
+        /**
+         * Рекурсивно проходим по объекту и сохраняем найденных родителей
+         * @param nodes - Узлы дерева
+         * @param level - Глубина дерева
+         */
+        const findParent = (nodes: TTree[], level = 1): void => {
             nodes.forEach((node) => {
                 if ('children' in node && node.children.length) {
                     nodeParents.push({
@@ -40,46 +70,26 @@ const DataBrowsing = () => {
         return nodeParents;
     };
 
-    const fetchData = async () => {
-        try {
-            const { data } = await axios.get('/api/data');
-            setTree(data.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
+    /**
+     * При получении дерева с сервера находим и сохраняем родительские элементы в state
+     */
     useEffect(() => {
-        if (!isLoggedIn()) {
-            navigate('/login');
-        }
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        setParents(findNodeParents());
+        const nodeParents = findNodeParents();
+        setParents(nodeParents);
     }, [tree]);
 
-    useEffect(() => {
-        const sorted = [...children].sort(order === 'asc' ? compareAsc : compareDesc);
-        setChildren(sorted);
-    }, [order]);
-
-    const handleClickParent = (key: string) => {
-        console.log(key);
-        const foundChildren = parents.find((parent) => parent.key === key).children;
-        const sorted = [...foundChildren].sort(order === 'asc' ? compareAsc : compareDesc);
-        setChildren(sorted);
-    };
-
-    const handleChangeSort = (event: ChangeEvent<HTMLSelectElement>) => {
-        setOrder(event.target.value as TOrder);
+    /**
+     * Обновляем state новым значением ключа
+     * @param key - Ключ родителя, по которому кликнули
+     */
+    const handleClickParent = (key: string): void => {
+        setParentKey(key);
     };
 
     return (
         <div>
             <Parents parents={parents} handleClick={handleClickParent} />
-            <Children children={children} handleChangeSort={handleChangeSort} order={order} />
+            <Children parents={parents} parentKey={parentKey} />
         </div>
     );
 };
