c5ab3f861abf50de2dbec392ae53d430b23ed76d
Frit027 <Frit_027@mail.ru>
2023-11-13 14:05:06 +0300

    added PrivateRoute

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './elements/header';
 import Main from './pages/main';
 import Login from './pages/login';
+import PrivateRoute from './pages/private-route';
 import DataBrowsing from './pages/data-browsing';
-import Header from './elements/header';
 
 const App = () => (
     <BrowserRouter>
         <Header />
         <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/browse" element={<DataBrowsing />} />
+            <Route path="login" element={<Login />} />
+            <Route
+                path="browse"
+                element={(
+                    <PrivateRoute>
+                        <DataBrowsing />
+                    </PrivateRoute>
+                )}
+            />
         </Routes>
     </BrowserRouter>
 );
