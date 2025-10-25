91a8d376c96fcb6d7857233549894139dc1b496f
Frit027 <Frit_027@mail.ru>
2025-10-25 14:18:12 +0300

    component declaration simplified

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -5,8 +5,6 @@ import { texts } from './constants';
  * Главная страница сайта
  * @constructor
  */
-const Main = () => (
-    <h2 className="text-center mt-3">{texts.text}</h2>
-);
+const Main = () => <h2 className="text-center mt-3">{texts.text}</h2>;
 
 export default Main;
